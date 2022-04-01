95f745f28dd519f26013dfc46b31554ea45390c6
NoFeel21 <93081703+NoFeel21@users.noreply.github.com>
2022-04-02 00:51:53 +0300

    Add files via upload

---
+++ 1.4/test1.4.js
@@ -0,0 +1,35 @@
+function fn1() {
+  var x = document.getElementById("bk1");
+  if (x.style.display === "block") {
+    x.style.display = "none"
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function fn2() {
+  var x = document.getElementById("bk2");
+  if (x.style.display === "block") {
+    x.style.display = "none"
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function fn3() {
+  var x = document.getElementById("bk3");
+  if (x.style.display === "block") {
+    x.style.display = "none"
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function fn4() {
+  var x = document.getElementById("bk4");
+  if (x.style.display === "block") {
+    x.style.display = "none"
+  } else {
+    x.style.display = "block";
+  }
+}
